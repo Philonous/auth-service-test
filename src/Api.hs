f0aefae4a3c7c83ec2529ec8ec0fa5d6a5f713bc
Philipp Balzarek <p.balzarek@googlemail.com>
2015-11-05 15:21:56 +0100

    revert to headers to transmit tokens/users (#2)

--- src/Api.hs
+++ src/Api.hs
@@ -15,7 +15,7 @@ import           Types
 
 type LoginAPI = "login"
               :> ReqBody '[JSON] Login
-              :> Post '[JSON] (Headers B64Token)
+              :> Post '[JSON] (Headers '[Header "X-Token" B64Token] B64Token)
 
 serveLogin :: ConnectionPool -> Config -> Server LoginAPI
 serveLogin pool conf loginReq = loginHandler
@@ -23,7 +23,7 @@ serveLogin pool conf loginReq = loginHandler
     loginHandler = do
         mbToken <- lift . runAPI pool conf $ login loginReq
         case mbToken of
-         Right tok -> return tok
+         Right tok -> return (addHeader tok tok)
          Left _e -> left err403
 
 type LogoutAPI = "logout"
@@ -37,8 +37,8 @@ serveLogout pool conf token = logoutHandler
         lift . runAPI pool conf $ logOut token
 
 type CheckTokenAPI = "checkToken"
-                  :> Capture "Token" B64Token
-                  :> Get '[JSON] ReturnUser
+                  :> Capture "token" B64Token
+                  :> Get '[JSON] (Headers '[Header "X-User" Username] ReturnUser)
 
 serveCheckToken :: ConnectionPool -> Config -> Server CheckTokenAPI
 serveCheckToken pool conf token = checkTokenHandler
@@ -51,7 +51,7 @@ serveCheckToken pool conf token = checkTokenHandler
              res <- lift . runAPI pool conf $ checkToken token
              case res of
               Nothing -> left err403
-              Just usr -> return $ ReturnUser usr
+              Just usr -> return $ (addHeader usr $ ReturnUser usr)
 
 type UserMirrorAPI = "showUser"
                    :> Header "X-User" Text
