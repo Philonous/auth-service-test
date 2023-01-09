aac528f3df4277878a2b3e6d92ab1afeae3059be
Jon Kristensen <jon@nejla.com>
2023-01-09 19:51:40 +0100

    Fix wrong hostname in proxy/run.sh (#161)

--- proxy/run.sh
+++ proxy/run.sh
@@ -24,7 +24,7 @@ m4 -DAUTH_SERVICE=auth-service-backend:80 \
    /nginx.conf.m4 \
    > /etc/nginx/nginx.conf
 
-until nc -z auth-service 80; do
+until nc -z auth-service-backend 80; do
     echo "Waiting for auth-service..."
     sleep 1
 done
